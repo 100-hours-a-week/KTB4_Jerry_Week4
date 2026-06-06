5ffbf91cca3983e2cb65456453b7ba7d0fd8adf4
yeobi <kimhyjjang@gmail.com>
2026-06-06 15:11:50 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/ktb/fullstack/talktalk/TalktalkApplication.java
@@ -0,0 +1,13 @@
+package ktb.fullstack.talktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalktalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalktalkApplication.class, args);
+    }
+
+}
